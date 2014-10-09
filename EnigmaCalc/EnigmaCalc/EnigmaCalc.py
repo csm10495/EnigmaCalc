ccc6155b6cd399288a29fc4f1411344402056cdf
csm10495 <csm10495@gmail.com>
2014-10-09 18:45:13 -0400

    Basic (sort of working GUI), some notes

--- EnigmaCalc/EnigmaCalc/EnigmaCalc.py
+++ EnigmaCalc/EnigmaCalc/EnigmaCalc.py
@@ -1,1 +1,32 @@
-print('Hello World')
+#This file is part of EnigmaCalc
+#Team Enigma - RPI SDD - Fall 2014
+
+from pylab import * 
+from Tkinter import *
+
+root = Tk()
+
+def callback():
+    x = arange(-10, 10, .01)
+    y = eval(e.get()) #Very very evil. We need to do this a lot better; should be a way to only
+                      #whitelist certain functions 
+
+                      #also need to handle simple user errors like using ^ instead of **
+                      #sinx instead of sin(x) 
+                      #Maybe just add '(' x ')' parenthesis to all x's?
+    print "Trying to Graph!"
+    plot(x,y)
+    show()
+
+# create window contents as children to root...
+w = Label(text="Input Function\n Y = ")
+w.pack()
+e = Entry()
+e.pack()
+
+e.delete(0, END)
+e.insert(0, "")
+b = Button(text="Graph!", command=callback)
+b.pack()
+
+root.mainloop()
