a4eee5dccafb6cd627002e5e3058e679d97e0bd3
Anli Ji <anli.azuresand@gmail.com>
2014-10-12 18:00:04 -0400

    Make something not crash

--- EnigmaCalc/EnigmaCalc/EnigmaCalc.py
+++ EnigmaCalc/EnigmaCalc/EnigmaCalc.py
@@ -12,6 +12,7 @@ def callback():
     x = arange(-10, 10, .01)
     
     function = e.get().replace("^", "**")
+    function = function + "+x-x"
     safe_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'cosh', 'degrees', 'e', 'exp', 'fabs', 'floor', 'fmod', 'frexp', 'hypot', 'ldexp', 'log', 'log10', 'modf', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', '+', '-', '*', '/', '**', 'x']
     safe_dict = dict([ (k, locals().get(k, None)) for k in safe_list ])
     y = eval(function, {"__builtins__":None}, safe_dict) 
