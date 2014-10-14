4e385684c42abe4a65c1a93ef63cdb760d46ffff
csm10495 <csm10495@gmail.com>
2014-10-14 18:52:02 -0400

    Made code much cleaner

--- EnigmaCalc/EnigmaCalc/EnigmaCalc.py
+++ EnigmaCalc/EnigmaCalc/EnigmaCalc.py
@@ -1,48 +1,72 @@
 #This file is part of EnigmaCalc
 #Team Enigma - RPI SDD - Fall 2014
 
-import pylab 
-from Tkinter import *
 
-root = Tk()
+import pylab     #graphing library, part of matplotlib
+import Tkinter   #GUI library, standard Python
 
 
-def callback():
-    x = pylab.arange(-10, 10, .01)
-    
-    function = e.get().replace("^", "**")
-    function = function + "+x-x"
+#call this once!
+#gets a dictionary of functions allowed to be called by eval
+def getSafeDict():
     safe_list = ['sin', 'cos', 'tan'] #todo -> add more functions from pylab to this list
     safe_dict = dict((k, getattr(pylab, k)) for k in safe_list)
 
     # ading some more things to safe_dict that are not in pylab
-    safe_dict['x'] = x
+    # we can't add x yet because it isn't set
     safe_dict['+'] = locals().get('+')
     safe_dict['-'] = locals().get('-')
     safe_dict['/'] = locals().get('/')
     safe_dict['*'] = locals().get('*')
     safe_dict['**'] = locals().get('**')
-    #
+
+    return safe_dict
+
+
+#graphs a function by grabbing from e.get()
+def graph(function_text, safe_dict):
+
+    x = pylab.arange(-10, 10, .01)
+    safe_dict['x'] = locals().get('x')
+
+    function = function_text.replace("^", "**")
+    function = function + "+x-x"
 
     y = eval(function, {"__builtins__":None}, safe_dict) 
 
-
     #sinx instead of sin(x) 
     #Maybe just add '(' x ')' parenthesis to all x's?
-
-    print "Trying to Graph!"
+    print "Graphing: Y =", function_text
     pylab.plot(x,y)
     pylab.show()
 
-# create window contents as children to root...
-w = Label(text = "Input Function\n Y = ")
-w.pack()
-e = Entry()
-e.pack()
 
-e.delete(0, END)
-e.insert(0, "")
-b = Button(text="Graph!", command=callback)
-b.pack()
+#function that needs to be run for the program to start (like a main)
+def startUp():
+    safe_dict = getSafeDict()
 
-root.mainloop()
+    # create window contents
+    root = Tkinter.Tk()
+
+    #instruction text
+    instr = Tkinter.Label(root, text = "Input Function")
+    instr.pack()
+
+    #Y = text
+    yequals = Tkinter.Label(root, text = "Y = ")
+    yequals.pack(side = Tkinter.LEFT)
+
+    #inputbox for function
+    graph_entry = Tkinter.Entry(root, text = "")
+    graph_entry.pack(side = Tkinter.LEFT)
+
+    #graph button
+    b = Tkinter.Button(root, text="Graph!", command = lambda: graph(graph_entry.get(), safe_dict))
+    b.pack()
+    
+    #mainloop needs to be run
+    #Every GUI is a loop...
+    root.mainloop()
+
+
+startUp()
